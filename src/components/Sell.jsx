0ba0cbaca40aff41cbf818187b726899a5b3cfc4
Kaveh Khorram <kavehkhorram@wlan806-977.resnet.cwu.edu>
2017-04-08 15:26:30 -0700

    notifications now implemented.

--- src/components/Sell.jsx
+++ src/components/Sell.jsx
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import TextField from 'material-ui/TextField';
-import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
+import { Card, CardHeader, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
 
 export default class Sell extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'amount': '',
+            'price': '',
+            'email': '',
+            open: false,
+            message: ''
+        }
+        this.updateAmount = this.updateAmount.bind(this);
+        this.updatePrice = this.updatePrice.bind(this);
+        this.updateEmail = this.updateEmail.bind(this);
+        this.makeSellPost = this.makeSellPost.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+    }
     render() {
+        const actions = [
+        <FlatButton
+            label="Okay"
+            primary={true}
+            keyboardFocused={false}
+            onTouchTap={this.handleClose}
+        />,
+        ];
+        console.log(JSON.stringify(this.state));
         return (
             <div>
+            <Dialog
+                title="Message"
+                actions={actions}
+                modal={false}
+                open={this.state.open}
+                onRequestClose={this.handleClose}
+            >{this.state.message}</Dialog>
                 <Header current={'sell'}/>
                 <div className="sell container">
                     <Card>
@@ -15,16 +48,63 @@ export default class Sell extends Component {
                             subtitle="Turn your unused Meal Points turn into $$."
                         />
                         <CardText>
-                            <TextField ref="amount" hintText="Amount of points?"/>
+                            <TextField onChange={this.updateAmount} hintText="Amount of points?"/>
                             <br/>
-                            <TextField ref="price" hintText="How much per point?"/>
+                            <TextField onChange={this.updatePrice} hintText="How much per point?"/>
                             <br/>
-                            <TextField ref="email" hintText="CWU E-mail"/>
+                            <TextField onChange={this.updateEmail} hintText="CWU E-mail"/>
                             <br/>
+                            <FlatButton onTouchTap={this.makeSellPost} label="submit" backgroundColor={'rgb(0,188,212'}/>
                         </CardText>
                     </Card>
                 </div>
             </div>
         )
+    }
+
+    updateAmount(event, newValue) {
+        this.setState({
+            amount: newValue
+        })
+    }
+
+    updatePrice(event, newValue) {
+        this.setState({
+            price: newValue
+        })
+    }
+
+    updateEmail(event, newValue) {
+        this.setState({
+            email: newValue
+        })
+    }
+
+    makeSellPost() {
+        fetch('https://d15c8e70.ngrok.io/cwu', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify(this.state)
+        })
+        .then((response) => response.json())
+        .then((responseJson) => {
+            // If the response is valid, redirect to -
+            // - buy page and give toast.
+            // Otherwise, empty form and notify user.
+            console.log(responseJson)
+            this.setState({
+                open: true,
+                message: responseJson.message
+            })
+        })
+    }
+    
+    handleClose() {
+        this.setState({
+            open: false
+        })
     }
 }
